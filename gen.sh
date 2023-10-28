60937a792565c5ee8ee046388e3da9117ee823f6
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2023-10-28 11:40:01 +0330

    Better generator script

--- gen.sh
+++ gen.sh
@@ -3,7 +3,7 @@
 # Extract frames
 rm -r frames
 mkdir frames
-ffmpeg -i payan.mp4 -vf "scale=200:-2,fps=10" frames/%04d.jpg
+ffmpeg -i $1 -vf "scale=200:-2,fps=10" frames/%04d.jpg
 
 # Exctract bits
 python3 extract.py 10
